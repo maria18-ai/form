4d8c82ff87ec42a8d4c1e7c51e8bc9d975fb0c59
Maria Teodoro <maduteodoro4@gmail.com>
2024-01-23 19:09:11 -0300

    Estilo dos passos e icones finalizados

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import {FiSend} from "react-icons/fi"
 import UserForm from './components/UserForm';
 import ReviewForm from './components/ReviewForm';
 import Thanks from './components/Thanks';
-
+import Steps from './components/Steps';
 
 // hooks 
 import { useForm } from './hooks/useForm';
+
 
 function App() {
 
@@ -24,7 +25,7 @@ function App() {
       </div>
 
       <div className="form-container">
-        <p>etapas</p>
+        <Steps currentStep={currentStep} />
 
         <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
