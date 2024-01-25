86693ab1463085f1276811bed072797fba514ae7
Maria Teodoro <maduteodoro4@gmail.com>
2024-01-25 15:10:46 -0300

    Ultimo estilo aplicado

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import {
+    BsFillEmojiHeartEyesFill, 
+    BsFillEmojiSmileFill, 
+    BsEmojiNeutralFill, 
+    BsFillEmojiFrownFill
+} from "react-icons/bs"
+
+import './ReviewForm.css'
 
 export default function ReviewForm() {
     return (
-        <div>
-            <h2>ReviewForm</h2>
+        <div className='review-form'>
+            <div className="form-control score-container">
+
+                <label className='radio-container'>
+                    <input type="radio" value="unsatisfied" name='review' required/>
+                    <BsFillEmojiFrownFill />
+                    <p>Insastifeito</p>
+                </label>
+
+                <label className='radio-container'>
+                    <input type="radio" value="neutral" name='review' required/>
+                    <BsEmojiNeutralFill />
+                    <p>Poderia ser melhor</p>
+                </label>
+
+                <label className='radio-container'>
+                    <input type="radio" value="satisfied" name='review' required/>
+                    <BsFillEmojiSmileFill />
+                    <p>Satisfeito</p>
+                </label>
+
+                <label className='radio-container'>
+                    <input type="radio" value="very_satisfield" name='review' required/>
+                    <BsFillEmojiHeartEyesFill />
+                    <p>Muito satisfeito</p>
+                </label>
+            </div>
+            <div className="form-control">
+                <label htmlFor="comment">Comentário:</label>
+                <textarea name="comment" 
+                id="comment" 
+                placeholder="Conte como foi sua experiência com o produto..." 
+                required
+                ></textarea>
+            </div>
         </div>
     )
 }
